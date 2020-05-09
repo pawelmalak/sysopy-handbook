67f3d83c220ce1c04826cfdf63865c58fd0b324a
unknown <pawel999@icloud.com>
2020-05-09 13:59:12 +0200

    v0.1.1 - work env.

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+
+function bundleCss() {
+  return gulp.src('src/css/**/*.css')
+  .pipe(concat('bundle.css'))
+  .pipe(gulp.dest('dist'));
+}
+
+exports.default = series(bundleCss);
