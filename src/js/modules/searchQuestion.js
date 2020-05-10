8a1a10464bf36028beafaf6fa0e1307b95e3bbed
unknown <pawel999@icloud.com>
2020-05-11 00:13:50 +0200

    v0.5.0 - filters implemented

--- src/js/modules/searchQuestion.js
+++ src/js/modules/searchQuestion.js
@@ -8,19 +8,61 @@ export const searchQuestion = (questionsArray) => {
   searchBar.addEventListener('keyup', (e) => {
     
     const regexPattern = new RegExp(e.target.value, 'i');
-    let searchResults;
+    let searchResults = [];
 
-    if (getFilters('searchRange')) {
+    // check if any chapter filter is active
+    if (Object.values(getFilters('chapters')).reduce((a, x) => a += x)) {
+
+      let chapters = getFilters('chapters');
+      const toBeIncluded = [];
+      
+      // push all id's to be included in search
+      for (let i = 0; i < Object.keys(chapters).length; i++) {
+        if (Object.values(chapters)[i]) toBeIncluded.push(Object.keys(chapters)[i]);
+      }
+
+      if (toBeIncluded.length != 0) {
+        searchResults = questionsArray.filter(({ question_chapter }) => toBeIncluded.includes(question_chapter));
+      }
+
+    }
+
+    // check if any group should be excluded from search
+    if (Object.values(getFilters('groups')).reduce((a, x) => a += x)) {
+      
+      let groups = getFilters('groups');
+      const toBeIncluded = [];
+      
+      // push all id's to be included in search
+      for (let i = 0; i < Object.keys(groups).length; i++) {
+        if (Object.values(groups)[i]) toBeIncluded.push(Object.keys(groups)[i]);
+      }
+
+      if (searchResults.length == 0 && toBeIncluded.length != 0) {
+        searchResults = questionsArray.filter(({ question_group }) => toBeIncluded.includes(question_group));
+      }
+      else {
+        searchResults = searchResults.filter(({ question_group }) => toBeIncluded.includes(question_group));
+      }
+
+    }
+
+    // check if search should be performed on title only
+    if (searchResults.length == 0 && getFilters('searchRange')) {
       searchResults = questionsArray.filter(({ question_title }) => regexPattern.test(question_title));
     }
+    else if (getFilters('searchRange')) {
+      searchResults = searchResults.filter(({ question_title }) => regexPattern.test(question_title));
+    }
     else {
-      const combinedFieldsArray = questionsArray.map((question) => {
+      const combinedFieldsArray = searchResults.map((question) => {
         question.combined = ''.concat(question.question_title).concat(question.question_body);
         return question;
       });
       searchResults = combinedFieldsArray.filter(({ combined }) => regexPattern.test(combined));
     }
     
+    console.log(searchResults);
     populateQuestions(searchResults);
 
   });
